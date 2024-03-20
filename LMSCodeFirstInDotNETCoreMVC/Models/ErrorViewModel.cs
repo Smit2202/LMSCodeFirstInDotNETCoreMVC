2f781e50dbc06afc1bd7e16eecaf3a935a3bbfb9
Smit2202 <smit.bhanushali@arishti.com>
2024-03-20 12:21:14 +0530

    Add project files.

---
+++ LMSCodeFirstInDotNETCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LMSCodeFirstInDotNETCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
